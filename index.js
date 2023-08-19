2c01006c4a76da7b963b8b3a11ac033bdff0e921
brendokht <103231108+brendokht@users.noreply.github.com>
2023-08-19 00:43:32 -0400

    Completed etch-a-sketch.

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
-const square = document.querySelector('.square');
+const width = 600;
+const height = 600;
+let squareSize = 16;
 
-for(var i = 0; i < 16; ++i) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for(var j = 0; j < 16; ++j) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        row.appendChild(pixel);
+// Create square based on given variables
+function changeSquareSize(size) {
+    const square = document.querySelector('.square');
+    
+    for(var i = 0; i < size; ++i) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for(var j = 0; j < size; ++j) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.style.width = `${width/size}px`;
+            pixel.style.height = `${height/size}px`;
+            pixel.addEventListener('mouseover', (e) => {
+                pixel.classList.add('toggled');
+            });
+            row.appendChild(pixel);
+        }
+        square.appendChild(row);
     }
-    square.appendChild(row);
 }
 
-const pixels = document.querySelectorAll('.pixel');
+// Add event listener for button to change size
+const sizeBtn = document.querySelector('.button');
+sizeBtn.addEventListener('click', (e) => {
+    let newSquareSize = null;
+    let valid = false;
+    let promptMsg = 'Choose a size for the square between 16x16 and 128x128'
+    while(!valid) {
+        newSquareSize = prompt(promptMsg);
 
-pixels.forEach(pixel => {
-    pixel.addEventListener('mouseover', (e) => {
-        pixel.classList.add('toggled');
+        if((newSquareSize = Math.floor(parseInt(newSquareSize)))) {
+            if(newSquareSize < 16 || newSquareSize > 128) {
+                promptMsg = 'Square size invalid please make sure it is between 16 and 128'
+                valid = false;
+            }
+            else {
+                valid = true;
+            }
+        }
+        else {
+           promptMsg = 'Please choose an integer between 16 and 128'
+            valid = false; 
+        }
+    }
+    
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+        row.remove();
     });
-})
+    changeSquareSize(newSquareSize);
+});
+
+changeSquareSize(squareSize);
