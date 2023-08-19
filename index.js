fa4ebe60f7a8df1d78a26a86b427035ca17ed990
brendokht <103231108+brendokht@users.noreply.github.com>
2023-08-18 23:57:08 -0400

    Set up 16x16 square and footer on index page

---
+++ index.js
@@ -0,0 +1,12 @@
+const square = document.querySelector('.square');
+
+for(var i = 0; i < 16; ++i) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for(var j = 0; j < 16; ++j) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        row.appendChild(pixel);
+    }
+    square.appendChild(row);
+}
